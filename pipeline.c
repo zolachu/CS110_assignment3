be1558de2908edcd906b1614df018298cde14bf6
CS110 tools <cs110@cs.stanford.edu>
2020-05-06 13:08:08 -0700

    This project passes 1 of the 5 default sanity check cases.

--- pipeline.c
+++ pipeline.c
@@ -11,5 +11,30 @@
 #include "fork-utils.h"  // this has to be the last #include'd statement in the file
 
 void pipeline(char *argv1[], char *argv2[], pid_t pids[]) {
-  fprintf(stderr, "pipeline: not yet implemented.\n");
+  pid_t pid1, pid2;
+  int fds[2];
+
+  pipe(fds);
+
+  pid1 = fork();
+  if (pid1 == 0) {
+    close(fds[0]);
+    dup2(fds[1], STDOUT_FILENO);
+    close(fds[1]);
+    execvp(argv1[0], argv1);
+  } else {
+    pids[0] = pid1;
+  }
+
+  pid2 = fork();
+  if (pid2 == 0) {
+    close(fds[1]);
+    dup2(fds[0], STDIN_FILENO);
+    close(fds[0]);
+    execvp(argv2[0], argv2);
+  } else {
+    pids[1] = pid2;
+  }
+  close(fds[0]);
+  close(fds[1]);
 }
--- subprocess.cc
+++ subprocess.cc
@@ -8,6 +8,58 @@
 #include "fork-utils.h" // this has to be the very last #include statement in this .cc file!
 using namespace std;
 
+void try_pipe(int* fd);
+void try_execvp(char* firstChar, char* argv[]);
+void try_close(int fd);
+void try_dup2(int fd, int fd1);
+
+
 subprocess_t subprocess(char *argv[], bool supplyChildInput, bool ingestChildOutput) throw (SubprocessException) {
-  throw SubprocessException("subprocess: not yet implemented.");
+
+  int fds1[2];
+  int fds2[2];
+  pipe(fds1);
+  pipe(fds2);
+  subprocess_t sp = {fork(), kNotInUse, kNotInUse};
+
+  if (sp.pid == 0){
+    if (supplyChildInput) {
+      try_dup2(fds1[0], STDIN_FILENO);
+    }
+    try_close(fds1[0]);
+    try_close(fds1[1]);
+    if (ingestChildOutput) {
+      try_dup2(fds2[0], STDIN_FILENO);
+    }
+    try_close(fds2[0]);
+    try_close(fds2[1]);
+
+    try_execvp(argv[0], argv);
+  } else {
+    try_close(fds1[0]);
+    try_close(fds2[1]);
+    if (supplyChildInput) {
+      sp.supplyfd = fds1[1];
+    }
+    if (ingestChildOutput) {
+      sp.ingestfd =  fds2[0];
+    }
+  }
+  return sp;
+}
+
+void try_pipe(int* fd) {
+  if (pipe(fd) < -1) throw SubprocessException("failed to pipe");
+}
+
+void try_execvp(char* firstChar, char* argv[]) {
+  if (execvp(firstChar, argv) < -1) throw SubprocessException("failed to execute the command");
+}
+
+void try_close(int fd) {
+  if (close(fd) == -1) throw SubprocessException("failed to close the file descriptor");
+}
+
+void try_dup2(int fd, int fd1) {
+  if (dup2(fd, fd1) == -1) throw SubprocessException("failed to duplicate the filedescriptor");
 }
