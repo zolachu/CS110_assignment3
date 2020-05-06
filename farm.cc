643f9a7ab0a0eb249362f1539570d8ff15c314f4
CS110 tools <cs110@cs.stanford.edu>
2020-05-06 13:19:24 -0700

    This project passes 3 of the 5 default sanity check cases.

--- farm.cc
+++ farm.cc
@@ -21,41 +21,103 @@ struct worker {
 };
 
 static const size_t kNumCPUs = sysconf(_SC_NPROCESSORS_ONLN);
-// restore static keyword once you start using these, commented out to suppress compiler warning
-/* static */ vector<worker> workers(kNumCPUs);
-/* static */ size_t numWorkersAvailable = 0;
+static vector<worker> workers(kNumCPUs);
+static size_t numWorkersAvailable = 0;
 
-static void markWorkersAsAvailable(int sig) {}
-
-// restore static keyword once you start using it, commented out to suppress compiler warning
-/* static */ const char *kWorkerArguments[] = {"./factor.py", "--self-halting", NULL};
-static void spawnAllWorkers() {
-  cout << "There are this many CPUs: " << kNumCPUs << ", numbered 0 through " << kNumCPUs - 1 << "." << endl;
-  for (size_t i = 0; i < kNumCPUs; i++) {
-    // cout << "Worker " << workers[i].sp.pid << " is set to run on CPU " << i << "." << endl;
+static void markWorkersAsAvailable(int sig) {
+  while(true) {
+    pid_t pid = waitpid(-1, NULL, WNOHANG|WUNTRACED);
+    if(pid <= 0)  break;
+    for(size_t worker = 0; worker < kNumCPUs; worker++) {
+      if(workers[worker].sp.pid == pid) {
+	workers[worker].available = true;
+	numWorkersAvailable++;
+	break;
+      }
+    }
   }
 }
 
-// restore static keyword once you start using it, commented out to suppress compiler warning
-/* static */ size_t getAvailableWorker() {
-  return 0;
+static const char *kWorkerArguments[] = {"./factor.py", "--self-halting", NULL};
+static void spawnAllWorkers() {
+  cout << "There are this many CPUs: " << kNumCPUs << ", numbered 0 through " << kNumCPUs - 1 << "." << endl;
+  for (size_t i = 0; i < kNumCPUs; i++) {
+    cpu_set_t cpus;
+    CPU_ZERO(&cpus);
+    CPU_SET(i, &cpus);
+
+    workers[i] = worker((char**)kWorkerArguments);
+    sched_setaffinity(workers[i].sp.pid, sizeof(cpu_set_t), &cpus);
+    cout << "Worker " << workers[i].sp.pid << " is set to run on CPU " << i << "." << endl;
+  }
+}
+
+static size_t getAvailableWorker() {
+  sigset_t additions, existingmask;
+  sigemptyset(&additions);
+  sigaddset(&additions, SIGCHLD);
+  sigprocmask(SIG_BLOCK, &additions, &existingmask);
+  while(numWorkersAvailable == 0) {
+    sigsuspend(&existingmask);
+  }
+  int availableWorkers = kNumCPUs;
+  for(size_t worker = 0; worker < kNumCPUs; worker++) {
+    if(workers[worker].available) {
+      availableWorkers = worker;
+      break;
+    }
+  }
+
+  sigprocmask(SIG_UNBLOCK, &additions, NULL);
+  return availableWorkers;
 }
 
 static void broadcastNumbersToWorkers() {
-  while (true) {
+  while(true) {
     string line;
     getline(cin, line);
     if (cin.fail()) break;
     size_t endpos;
-    /* long long num = */ stoll(line, &endpos);
+    long long num = stoll(line, &endpos);
     if (endpos != line.size()) break;
-    // you shouldn't need all that many lines of additional code
+
+    size_t numWorkers = getAvailableWorker();
+    if(numWorkers < kNumCPUs) {
+      workers[numWorkers].available = false;
+      numWorkersAvailable--;
+      dprintf(workers[numWorkers].sp.supplyfd, "%lld\n", num);
+      kill(workers[numWorkers].sp.pid, SIGCONT);
+    }
   }
 }
 
-static void waitForAllWorkers() {}
+static void waitForAllWorkers() {
+  sigset_t additions, existingmask;
+  sigemptyset(&additions);
+  sigaddset(&additions, SIGCHLD);
+  sigprocmask(SIG_BLOCK, &additions, &existingmask);
+  while(numWorkersAvailable != kNumCPUs) {
+    sigsuspend(&existingmask);
+  }
+  sigprocmask(SIG_UNBLOCK, &additions, NULL);
+}
 
-static void closeAllWorkers() {}
+static void closeAllWorkers() {
+  signal(SIGCHLD, SIG_DFL);
+
+  for(size_t worker = 0; worker < kNumCPUs; worker++) {
+    close(workers[worker].sp.supplyfd);
+    kill(workers[worker].sp.pid, SIGCONT);
+  }
+
+  for(size_t i = 0; i < kNumCPUs; i++) {
+    while(true) {
+      int status;
+      waitpid(workers[i].sp.pid, &status, 0);
+      if(WIFEXITED(status)) break;
+    }
+  }
+}
 
 int main(int argc, char *argv[]) {
   signal(SIGCHLD, markWorkersAsAvailable);
